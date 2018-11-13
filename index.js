5036b26f2c39de183ada761a399c295ffbf0137d
Roel Antonisse <roel.antonisse@gmail.com>
2018-11-13 14:24:09 +0100

    Update command;
    - Add option to add file of url list
    - Add option to test single or sample of rules
    - Add folder structure to domain to validate multiple pages
    - Add a nicedate to saved reports

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-const fs = require('fs');
+const { readFileSync } = require('fs');
 
 const commander = require('commander');
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
 
 const validate = require('./lib/commands/validate.js');
 
 const options = {};
+let urls = [];
 
 commander
   .version(pkg.version)
   .usage('<url...>')
   .description('Validate an url or list of urls with @Siteimprove/Alfa')
   .option('-o, --output <path>', 'Define a path to save reports to')
+  .option('-r, --rules <rulekey1,rulekey2,...rulekeyn>', 'Pass one or more rule keys as a list to validate.')
   .option('--no-summary', 'Do not create a summary.')
-  .option('--no-save', 'Do not save a report');
+  .option('--no-save', 'Do not save a report')
+  .option('-u, --url-list <file>', 'Use a textfile with urls as input');
 
 commander.parse(process.argv);
+
+if (commander.urlList) {
+  readFileSync(commander.urlList, 'utf8')
+    .split(/\s+/g)
+    .forEach((url) => {
+      if (url.length > 0) {
+        urls.push(url);
+      }
+    });
+} else {
+  urls = urls.concat(commander.args);
+}
 
 options.output = commander.output;
 options.summary = commander.summary;
 options.save = commander.save;
+options.rules = commander.rules && commander.rules.split(',').map(rule => rule.trim().toUpperCase());
 
-if (commander.args.length === 0) {
+if (urls.length === 0) {
   console.error('Required at least one url');
   process.exit(-1);
 }
 
-if (commander.args.length > 0) {
-  commander.args.forEach((url) => {
-    validate(url, options);
+if (urls.length > 0) {
+  urls.forEach(async (url) => {
+    await validate(url, options);
   });
 }
