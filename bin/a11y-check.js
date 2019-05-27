d08b73ac2da316da3ff7b5454bcd63b441802888
Roel Antonisse <roel.antonisse@gmail.com>
2019-05-27 16:43:43 +0200

    Add command executable to lib/index.js as a11yCheck
    (With basic arguments check)

--- bin/a11y-check.js
+++ bin/a11y-check.js
@@ -3,11 +3,12 @@ const command = require('commander');
 const fs = require('fs');
 const path = require('path');
 
+const a11yCheck = require('../lib/index.js');
+
 const PKG = JSON.parse(fs.readFileSync(
   path.resolve('package.json'),
   'utf8'
 ));
-
 
 command
   .version(`${PKG.name}: version ${PKG.version}`)
@@ -15,3 +16,19 @@ command
   .description(PKG.description);
 
 command.parse(process.argv);
+
+function getUrls(command) {
+  return command.args;
+}
+
+function hasUrls(command) {
+  return command.args.length > 0;
+}
+
+if (hasUrls(command)) {
+  a11yCheck(getUrls(command));
+}
+
+else {
+  console.log('[a11y-check]: Aborted. No arguments passed. Expected at least one argument.');
+}
