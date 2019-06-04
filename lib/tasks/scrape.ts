c1cc41600745cc42b4324237bdc0c9bfc5eecd38
Roel Antonisse <roel.antonisse@gmail.com>
2019-06-04 14:24:55 +0200

    Add options to scraper

--- lib/tasks/scrape.ts
+++ lib/tasks/scrape.ts
@@ -9,7 +9,13 @@ export default async function scrape(url: string): Promise<act.Aspects> {
   log(`Scrape ${url}`);
 
   try {
-    const result = await scraper.scrape(url);
+    const result = await scraper.scrape(url, {
+      viewport: {
+        width: 800,
+        height: 600,
+      },
+      timeout: 30000,
+    });
     log(`Scrape successfull`);
     return result;
   } catch (error) {
